fc71928a4e421e6e032a80517e31c0353306a0ad
Rayirthiscool <rayirthsinha09@gmail.com>
2026-05-01 21:07:32 +0530

    done with letter replacement

--- hangman/hangman-game.py
+++ hangman/hangman-game.py
@@ -5,18 +5,21 @@ chosen_word = random.choice(word_list)
 print(chosen_word)
 
 # TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
-placeholder = ""
-for position in range(1, len(chosen_word)):
-    placeholder += "_"
-print(placeholder)
+    
 
 
 guess = input("Guess a letter: ").lower()
 
 # TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
 
+display = ""
+
 for letter in chosen_word:
     if letter == guess:
-        print("Right")
+        display += letter
     else:
-        print("Wrong")
+        display += "_"
+
+print(display )
+
+
